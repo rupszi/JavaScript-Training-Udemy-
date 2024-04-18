1c011f46a13d44166d8f876a6a5b65822d495c57
Pal Megyes <ift@Pals-MacBook-Pro.local>
2024-04-18 16:38:16 +0100

    assignment section for variables added

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,39 +1,45 @@
-let js = "amazing";
-//if (js === "amazing") alert("JavaScript is FUN!");
-console.log(34 + 54 - 42 + 5)
+// let js = "amazing";
+// //if (js === "amazing") alert("JavaScript is FUN!");
+// console.log(34 + 54 - 42 + 5)
 
-console.log("Jonas");
-console.log(23);
+// console.log("Jonas");
+// console.log(23);
 
-let firstName = "Jonas"
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Jonas"
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-// Assignment for the Values and Variables lecture
+// // Assignment for the Values and Variables lecture
 
-let country = "Hungary";
-let continent = "Europe";
-let population = "9.8 million";
+// let country = "Hungary";
+// let continent = "Europe";
+// let population = "9.8 million";
 
-console.log(continent);
-console.log(country);
-console.log(population);
+// console.log(continent);
+// console.log(country);
+// console.log(population);
 
 
 // Data Types
 
-let javascriptIsFun = true;
+// let javascriptIsFun = true;
 
-console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 
-let year;
-console.log(year);
-year = 1991;
-console.log(typeof year);
+// let year;
+// console.log(year);
+// year = 1991;
+// console.log(typeof year);
 
-// DataTypes assignment
+// // DataTypes assignment
 
-let isIsland = true
-let language;
-console.log(population, country, isIsland, language);
+// let isIsland = true
+// let language;
+// console.log(population, country, isIsland, language);
+
+// Variable type assignment
+
+// language = hungarian;
+// const birthYear = 1984;
+
