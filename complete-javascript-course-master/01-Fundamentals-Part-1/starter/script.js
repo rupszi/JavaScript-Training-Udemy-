d060a512615aae4b504df784eb56c50b8b671cda
Pal Megyes <ift@Pals-MacBook-Pro.local>
2024-04-18 11:04:32 +0100

    printed the firstName variable to console line added

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,9 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+//if (js === "amazing") alert("JavaScript is FUN!");
 console.log(34 + 54 - 42 + 5)
 
 console.log("Jonas");
+console.log(23);
+
+let firstName = "Jonas"
+console.log(firstName);
