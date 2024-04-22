ef4a0b3821bc90e8f459e701ecf7bd057a999e81
Pal Megyes <ift@Pals-MacBook-Pro.local>
2024-04-22 10:34:14 +0100

    added switch statement and also rewrote the code to if else

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -190,3 +190,46 @@
 // } else {
 //     console.log("Someone else should drive!")
 // } 
+// Switch statement
+
+// const day = "wednesday"; // day === "monday"
+
+// switch (day) {
+//     case 'monday': //day === monday
+//         console.log('Plan course structure');
+//         console.log('Go to coding meetup')
+//         break;
+//     case 'tuesday':
+//         console.log('Prepare theory videos');
+//         break;
+//     case 'wednesday':
+//     case 'thursday':
+//         console.log('write video examples');
+//         break;
+//     case 'friday':
+//         console.log('Record videos');
+//         break;
+//     case 'saturday':
+//     case 'sunday':
+//         console.log('Enjoy the weekend');
+//         break;
+//     default:
+//         console.log('Not a valid day!');
+//         break;
+// }
+
+// if (day === 'monday') {
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+// } else if (day == "tuesday") {
+//     console.log('Prepare theory videos');
+// } else if (day == 'wednesday' || 'thursday') {
+//     console.log('Write video examples');
+// } else if (day === 'friday') {
+//     console.log('Record videos');
+// } else if (day === 'saturday' || 'sunday') {
+//     console.log('Enjoy the weekend');
+// } else {
+//     console.log('Not a valid day!');
+// }
+
