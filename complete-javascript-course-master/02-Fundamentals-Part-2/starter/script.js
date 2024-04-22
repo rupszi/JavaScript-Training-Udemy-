ac56529f51d3252d8136e14657e815d27b0da5ed
Pal Megyes <ift@Pals-MacBook-Pro.local>
2024-04-22 15:42:49 +0100

    learned strict mode

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,4 +1,13 @@
-'use strict'
+// Strict mode
 
-let hasDriversLicense = false;
-const passTest = true;
+// 'use strict';
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log("I can drive :D")
+
+// const private = 3;
+// const interface = 3;
+
