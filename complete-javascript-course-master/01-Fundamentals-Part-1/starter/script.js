2f71ad6eec2b3c7b86922eecc6b27642e83f6225
Pal Megyes <ift@Pals-MacBook-Pro.local>
2024-04-22 11:06:54 +0100

    added conditional operator

--- complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -233,3 +233,12 @@
 //     console.log('Not a valid day!');
 // }
 
+// Conditional operator
+
+const age = 23;
+// age >= 18 ? console.log("I like to drink wine :)") : console.log("I like to drink water :)");
+
+const drink = age >= 18 ? "wine" : 'water';
+console.log(drink);
+console.log(`I like to drink ${age >= 18 ? "wine" : 'water'}`);
+
