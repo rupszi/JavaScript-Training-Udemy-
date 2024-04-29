a0451c8f77b4633bb98b67f30054a5ebd5399d97
Pal Megyes <ift@Pals-MacBook-Pro.local>
2024-04-29 11:05:17 +0100

    Arrow functions section completed

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -36,7 +36,7 @@
 
 // Function declarations and expresions
 
-//Function Declaration;
+// // Function Declaration;
 
 // function calcAge1(birthYear) {
 //     // const age = 2027 - birthYear;
@@ -50,10 +50,24 @@
 // // Function expression;
 
 // const calcAge2 = function (birthYear) {
-//     return2037 - birthYear;
+//     return 2037 - birthYear;
 // }
 
 // const age2 = calcAge2(1991);
 // console.log(age1, age2);
 
-// Arrow Functions
+// // Arrow Functions
+
+// const calcAge3 = birthYear => 2037 - birthYear;
+// const age3 = calcAge3(1991);
+// console.log(age3);
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//     const age = 2037 - birthYear;
+//     const retirement = 65 - age;
+//     // return retirement;
+//     return `${firstName} retires in ${retirement} years.`;
+// }
+
+// console.log(yearsUntilRetirement(1991, 'Jonas'));
+// console.log(yearsUntilRetirement(1980, 'Bob'));
