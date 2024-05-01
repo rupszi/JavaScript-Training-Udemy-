5599d120e9b7a11ba1c2ebc8fce2794aec018e93
Pal Megyes <ift@Pals-MacBook-Pro.local>
2024-05-01 12:02:41 +0100

    refactored sample function

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -84,3 +84,18 @@
 // }
 
 // console.log(fruitProcessor(2, 3));
+
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+    return retirement;
+    // return firstName} retires in ${retirement} years.`;
+}
+
+console.log(yearsUntilRetirement(1991, 'John'));
