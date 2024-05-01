08ab32610bc09731e2ad30c4cd1adc09c8f99a46
Pal Megyes <ift@Pals-MacBook-Pro.local>
2024-05-01 11:43:04 +0100

    Finished Functions section part-1

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -71,3 +71,16 @@
 
 // console.log(yearsUntilRetirement(1991, 'Jonas'));
 // console.log(yearsUntilRetirement(1980, 'Bob'));
+
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+// }
+
+// function fruitProcessor(apples, oranges) {
+//     const applePieces = cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
+//     const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} piece of oranges.`;
+//     return juice;
+// }
+
+// console.log(fruitProcessor(2, 3));
