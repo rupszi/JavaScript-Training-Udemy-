93726ce0d5f66b056fc20ad836c05e80583b60fa
Pal Megyes <ift@Pals-MacBook-Pro.local>
2024-05-02 12:07:04 +0100

    new lines added

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -115,7 +115,13 @@ const friend1 = 'Michael';
 const friend2 = 'Steven';
 const friend3 = 'Peter';
 
+// Arrays Literal Syntax
 const friends = ['Michael', 'Steven', 'Peter'];
 console.log(friends);
 
 const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[2]);
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
