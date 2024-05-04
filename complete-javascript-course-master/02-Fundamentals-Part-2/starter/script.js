c623deea72b19f61b6094b0ad17dd884f4f6d209
Pal Megyes <ift@Pals-MacBook-Pro.local>
2024-05-04 21:50:57 +0200

    working with object operators

--- complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -185,3 +185,20 @@ const jonas = {
     job: 'teacher',
     friends: ['Michael', 'Peter', 'Steven']
 };
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+const interestedIn = prompt('What do you want to know about Jonas? Choose between firstname, lastName, age, job, and friends');
+
+console.log(interestedIn);
+console.log(jonas[interestedIn]);
+
+
+
+
